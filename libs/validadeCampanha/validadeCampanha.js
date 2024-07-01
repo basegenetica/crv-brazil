7dd5ceea884183d8186b147b118c2baadb74c4b8
Natanzin <natan.silva.judo@gmail.com>
2024-07-01 14:05:16 -0300

    inserção da lógica de desativar botão após o vencimento da campanha

--- libs/validadeCampanha/validadeCampanha.js
+++ libs/validadeCampanha/validadeCampanha.js
@@ -2,15 +2,18 @@ document.addEventListener("DOMContentLoaded", function() {
     const campanhas = document.querySelectorAll('.campanha');
     const hoje = new Date();
     
-    campanhas.forEach(campanha => {
+    campanhas.forEach((campanha, index) => {
         const validade = new Date(campanha.dataset.validade);
         const diffTime = validade - hoje;
         const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
         const icon = campanha.querySelector('.icon');
+        const button = document.querySelectorAll('.btn-campanha')[index];
         
         if (diffDays < 0) {
             campanha.classList.add('expired');
             icon.classList.add('fas', 'fa-times-circle');
+            //button.classList.add('disabled');
+            button.setAttribute('disabled', 'true');
         } else if (diffDays <= 5) {
             campanha.classList.add('warning');
             icon.classList.add('fas', 'fa-clock');
